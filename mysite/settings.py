ff5fbf21300c8b62cea36477d2a217c692073180
anvorja <borja.carlos@correounivalle.edu.co>
2023-11-02 01:25:50 -0500

    allowed host & dockerfile set up

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,3 +100,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 # CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:6789']
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+}
